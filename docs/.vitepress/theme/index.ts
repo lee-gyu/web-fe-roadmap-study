cc70c44b27b983d120012ac7455b1c90ba802a5e
lee-gyu <leegyu4dev@gmail.com>
2026-07-12 13:48:55 +0900

    design: mono font change to 'Nanum Gothic Coding'

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,7 @@
 import DefaultTheme from 'vitepress/theme';
 import ReadableDocs from './ReadableDocs.vue';
+
+import '@fontsource/nanum-gothic-coding/400.css';
 import './custom.css';
 
 export default {
